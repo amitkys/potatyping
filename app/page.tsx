33f444169601794723f7287992540c7e02027be5
KYs <amitkys59@gmail.com>
2025-09-18 13:25:29 +0530

    setup shadcn UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import { ModeToggle } from "@/components/modeToggle";
+import { Button } from "@/components/ui/button";
 import prisma from "@/lib/db";
 
 export default async function Home() {
   const users = await prisma.user.findMany();
   return (
     <div>
+      <ModeToggle />
       we have {users.length} users
+      <Button className="" variant={"outline"} size={"icon"}>Save and next</Button>
     </div>
   );
 }
